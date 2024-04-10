8c3eef170e9fdd615ece1458f0433428de3904a0
Pooja Babusingh <68894211+PoojaBabusingh@users.noreply.github.com>
2024-04-10 11:52:51 +0530

    move func to utils

--- Sources/VcIssuance/JWTTrail.swift
+++ Sources/VcIssuance/JWTTrail.swift
@@ -5,80 +5,56 @@ public class JWTTrail {
     public let myHeader: Header
     public let myClaims: MyClaims
     public var myJWT: JWT<MyClaims>
-    
-    let pemString = """
-    -----BEGIN RSA PRIVATE KEY-----
-    MIICWwIBAAKBgQDdlatRjRjogo3WojgGHFHYLugdUWAY9iR3fy4arWNA1KoS8kVw
-    33cJibXr8bvwUAUparCwlvdbH6dvEOfou0/gCFQsHUfQrSDv+MuSUMAe8jzKE4qW
-    +jK+xQU9a03GUnKHkkle+Q0pX/g6jXZ7r1/xAK5Do2kQ+X5xK9cipRgEKwIDAQAB
-    AoGAD+onAtVye4ic7VR7V50DF9bOnwRwNXrARcDhq9LWNRrRGElESYYTQ6EbatXS
-    3MCyjjX2eMhu/aF5YhXBwkppwxg+EOmXeh+MzL7Zh284OuPbkglAaGhV9bb6/5Cp
-    uGb1esyPbYW+Ty2PC0GSZfIXkXs76jXAu9TOBvD0ybc2YlkCQQDywg2R/7t3Q2OE
-    2+yo382CLJdrlSLVROWKwb4tb2PjhY4XAwV8d1vy0RenxTB+K5Mu57uVSTHtrMK0
-    GAtFr833AkEA6avx20OHo61Yela/4k5kQDtjEf1N0LfI+BcWZtxsS3jDM3i1Hp0K
-    Su5rsCPb8acJo5RO26gGVrfAsDcIXKC+bQJAZZ2XIpsitLyPpuiMOvBbzPavd4gY
-    6Z8KWrfYzJoI/Q9FuBo6rKwl4BFoToD7WIUS+hpkagwWiz+6zLoX1dbOZwJACmH5
-    fSSjAkLRi54PKJ8TFUeOP15h9sQzydI8zJU+upvDEKZsZc/UhT/SySDOxQ4G/523
-    Y0sz/OZtSWcol/UMgQJALesy++GdvoIDLfJX5GBQpuFgFenRiRDabxrE9MNUZ2aP
-    FaFp+DyAe+b4nDwuJaW2LURbr8AEZga7oQj0uYxcYw==
-    -----END RSA PRIVATE KEY-----
-    """
-   public let privateKey : Data
-
+    public var jwkJson: [String: String]
+    public var jsonDataAsData: Data
+    public var jsonString: String
+    public let pemString = getPrivateKeyStringForSigning()
+    public let privateKey : Data
+//    public let localPrivateKey: Data
+//    public let localPublicKey: Data
     public let jwtSigner: JWTSigner
     public let signedJWT: String
     
     
+    let localPrivateKeyPath : URL
+    let localPrivateKey: Data
+    let localPublicKeyPath : URL
+    let localPublicKey: Data
+    
+    
     public init() throws {
-        myHeader = Header(typ: "openid4vci-proof+jwt")
+        jwkJson = getJwk()
+        jsonDataAsData = try JSONSerialization.data(withJSONObject: jwkJson, options: .prettyPrinted)
+        jsonString = String(data: jsonDataAsData, encoding: .utf8)!
+//        localPrivateKey = getLocalPrivateKey()
+//        localPublicKey = getLocalPublicKey()
+        
+        
+        
+        myHeader = Header(typ: "openid4vci-proof+jwt", jwk: jsonString)
         myClaims = MyClaims(iss: "Z8LJ_p0lkh0aS3q6CMGdrrXt2-9MHvGD9S8BYVo9Bw0",
                         iat: Date() ,
                         exp: Date(timeIntervalSinceNow: 3600),
                         aud: "https://github.com",
                         nonce: "qwertyuio")
         myJWT = JWT(header: myHeader, claims: myClaims)
-        
         privateKey = pemString.data(using: .utf8)!
         
-        jwtSigner = JWTSigner.rs256(privateKey: privateKey)
+        
+        localPrivateKeyPath = URL(fileURLWithPath: "/Users/poojababusing/privateKey.key")
+        localPrivateKey = try Data(contentsOf: localPrivateKeyPath, options: .alwaysMapped)
+        localPublicKeyPath = URL(fileURLWithPath: "/Users/poojababusing/privateKey.key.pub")
+        localPublicKey = try Data(contentsOf: localPublicKeyPath, options: .alwaysMapped)
+
+        
+        
+        jwtSigner = JWTSigner.rs256(privateKey: localPrivateKey)
+//        jwtSigner = JWTSigner.hs256(key: privateKey)
         signedJWT = try myJWT.sign(using: jwtSigner)
     }
-    
-    
-    
+
     public func printVal(){
-//        
-//        if let privateKeyData = Data(base64Encoded: privateKey) {
-//            if let privateKeyString = String(data: privateKeyData, encoding: .utf8) {
-//                print(">>>>>>>>>>>> ", privateKeyString)
-//            } else {
-//                print("Failed to convert data to string using UTF-8 encoding")
-//            }
-//        } else {
-//            print("Invalid base64 string")
-//        }
-//        print(">>>>> private key \(privateKey) and type \(type(of: privateKey))")
         print("this JWT Values are ")
-        print("this JWT Values are ", signedJWT)
+        print(signedJWT)
     }
-    
-    
-    
 }
-
-public struct MyClaims: Claims {
-    let iss: String
-    let iat: Date
-    let exp: Date
-    let aud: String
-    let nonce: String
-}
-
-//const payload = {
-//    iss: context.selectedIssuer.client_id,
-//    nonce: decodedToken.c_nonce,
-//    aud: context.selectedIssuer.credential_audience,
-//    iat: Math.floor(new Date().getTime() / 1000),
-//    exp: Math.floor(new Date().getTime() / 1000) + 18000,
-//  };
-
--- Sources/VcIssuance/Utils.swift
+++ Sources/VcIssuance/Utils.swift
@@ -1,4 +1,5 @@
 import Foundation
+import SwiftJWT
 
 public func isValidURL(_ urlString: String) throws {
     guard let url = URL(string: urlString) else {
@@ -24,4 +25,83 @@ enum endPointError: String,Error{
     case invalidUrl =  "Invalid URL string"
     case invalidScheme = "Invalid Scheme"
     case invalidFragment = "Invalid fragment"
+}
+
+public struct MyClaims: Claims {
+    let iss: String
+    let iat: Date
+    let exp: Date
+    let aud: String
+    let nonce: String
+}
+
+public func getPrivateKeyStringForVerifying() -> String {
+    return """
+        -----BEGIN RSA PRIVATE KEY-----
+        MIICWwIBAAKBgQDdlatRjRjogo3WojgGHFHYLugdUWAY9iR3fy4arWNA1KoS8kVw
+        33cJibXr8bvwUAUparCwlvdbH6dvEOfou0/gCFQsHUfQrSDv+MuSUMAe8jzKE4qW
+        +jK+xQU9a03GUnKHkkle+Q0pX/g6jXZ7r1/xAK5Do2kQ+X5xK9cipRgEKwIDAQAB
+        AoGAD+onAtVye4ic7VR7V50DF9bOnwRwNXrARcDhq9LWNRrRGElESYYTQ6EbatXS
+        3MCyjjX2eMhu/aF5YhXBwkppwxg+EOmXeh+MzL7Zh284OuPbkglAaGhV9bb6/5Cp
+        uGb1esyPbYW+Ty2PC0GSZfIXkXs76jXAu9TOBvD0ybc2YlkCQQDywg2R/7t3Q2OE
+        2+yo382CLJdrlSLVROWKwb4tb2PjhY4XAwV8d1vy0RenxTB+K5Mu57uVSTHtrMK0
+        GAtFr833AkEA6avx20OHo61Yela/4k5kQDtjEf1N0LfI+BcWZtxsS3jDM3i1Hp0K
+        Su5rsCPb8acJo5RO26gGVrfAsDcIXKC+bQJAZZ2XIpsitLyPpuiMOvBbzPavd4gY
+        6Z8KWrfYzJoI/Q9FuBo6rKwl4BFoToD7WIUS+hpkagwWiz+6zLoX1dbOZwJACmH5
+        fSSjAkLRi54PKJ8TFUeOP15h9sQzydI8zJU+upvDEKZsZc/UhT/SySDOxQ4G/523
+        Y0sz/OZtSWcol/UMgQJALesy++GdvoIDLfJX5GBQpuFgFenRiRDabxrE9MNUZ2aP
+        FaFp+DyAe+b4nDwuJaW2LURbr8AEZga7oQj0uYxcYw==
+        -----END RSA PRIVATE KEY-----
+        """
+}
+
+public func getPrivateKeyStringForSigning() -> String {
+    return """
+    -----BEGIN RSA PRIVATE KEY-----
+    MIICWwIBAAKBgQDdlatRjRjogo3WojgGHFHYLugdUWAY9iR3fy4arWNA1KoS8kVw
+    33cJibXr8bvwUAUparCwlvdbH6dvEOfou0/gCFQsHUfQrSDv+MuSUMAe8jzKE4qW
+    +jK+xQU9a03GUnKHkkle+Q0pX/g6jXZ7r1/xAK5Do2kQ+X5xK9cipRgEKwIDAQAB
+    AoGAD+onAtVye4ic7VR7V50DF9bOnwRwNXrARcDhq9LWNRrRGElESYYTQ6EbatXS
+    3MCyjjX2eMhu/aF5YhXBwkppwxg+EOmXeh+MzL7Zh284OuPbkglAaGhV9bb6/5Cp
+    uGb1esyPbYW+Ty2PC0GSZfIXkXs76jXAu9TOBvD0ybc2YlkCQQDywg2R/7t3Q2OE
+    2+yo382CLJdrlSLVROWKwb4tb2PjhY4XAwV8d1vy0RenxTB+K5Mu57uVSTHtrMK0
+    GAtFr833AkEA6avx20OHo61Yela/4k5kQDtjEf1N0LfI+BcWZtxsS3jDM3i1Hp0K
+    Su5rsCPb8acJo5RO26gGVrfAsDcIXKC+bQJAZZ2XIpsitLyPpuiMOvBbzPavd4gY
+    6Z8KWrfYzJoI/Q9FuBo6rKwl4BFoToD7WIUS+hpkagwWiz+6zLoX1dbOZwJACmH5
+    fSSjAkLRi54PKJ8TFUeOP15h9sQzydI8zJU+upvDEKZsZc/UhT/SySDOxQ4G/523
+    Y0sz/OZtSWcol/UMgQJALesy++GdvoIDLfJX5GBQpuFgFenRiRDabxrE9MNUZ2aP
+    FaFp+DyAe+b4nDwuJaW2LURbr8AEZga7oQj0uYxcYw==
+    -----END RSA PRIVATE KEY-----
+    """
+}
+
+
+public func getJwk() -> [String:String]{
+    return ["e": "AQAB", "kid": "8oGvmgZ445GTnX5yrE6MLopX8MxmEhXIqjDaBYgk3Wo", "kty": "RSA", "n": "rcLfGOWt34vk0OAyJtrTsvlnJlZWgnaGsScKxtHjtQ5VXREbnck8zQ0kRHdpmMvFVFWYHRmafsQBjm-eZQhcpjly_5EiuvwMwoHCTpdh6sb32yJ0Rw-xdMgBnliS-V80p6kHSeGRDXqjKk37wHo5FPX3ZTWhdYRK-Jl0jhJ3_Ma06pMz5vekId__-bWy-5Ok0tVPn9dbJDfltBXsaVUj-tYv0DoI4TZ9t1kJ7nSq0mDMLf7SHsAsd9l1YKebwnVEx26raHD9drf8rUp2Wu93A8xAiix0b2kOtHXkQrF7EUZHoerjhHD2zBibKQWqCYISHJBnvY_pGrFV7_ynuNvXEgMKr4IxL5uiVzzsF77pZxw-9aD8iRXqw0zgleNz38cJkqWl90V3-nSvL9WgMIixkMrzfo74mYQcS7mk4z00bprQEqp6AVlNKgrPEba56PdqBYi2jKAd086jUOznxcXRLR2jkMRR_PfRwtHhNqwNKpxZsxpvDOK7ejIN_ehnXjidVELDn5fraUBEPI6F3sg1njZkzSKNJAzAxt-mu-CjUJn3uoouujMSk1D5ExEO_nGC06pOVoRk3Yy9F6HCsWTM0Bcp9nAH_uB1r-54p3OXUiJxqv28b0eK_CPhRspgeiQMqSmaRxIBQ8RZO_jyh1sgsVqW_e15Uk3RH7qfDlwc7OM"]
+}
+
+
+
+public func getLocalPrivateKey() -> Data {
+    let privateKeyPath = URL(fileURLWithPath: "/Users/poojababusing/privateKey.key")
+    var privateKey:Data?
+    do {
+        privateKey = try Data(contentsOf: privateKeyPath, options: .alwaysMapped)
+    }
+    catch{
+        print("error in file")
+    }
+    return privateKey!;
+}
+
+public func getLocalPublicKey() -> Data {
+    let publicKeyPath = URL(fileURLWithPath: "/Users/poojababusing/privateKey.key.pub")
+    var publicKey:Data?
+    do {
+        publicKey = try Data(contentsOf: publicKeyPath, options: .alwaysMapped)
+    }
+    catch{
+        print("error in file")
+    }
+    return publicKey!;
 }
